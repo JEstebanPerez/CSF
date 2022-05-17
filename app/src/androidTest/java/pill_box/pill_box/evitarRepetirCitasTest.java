516656370ed830e084a4d7983a4d118d2bca606d
Ara <auroborealzufi@gmail.com>
2022-05-17 23:37:22 +0200

    evitar repetir test

--- app/src/androidTest/java/pill_box/pill_box/evitarRepetirCitasTest.java
+++ app/src/androidTest/java/pill_box/pill_box/evitarRepetirCitasTest.java
@@ -1,28 +1,36 @@
 package pill_box.pill_box;
 
-import org.junit.runner.RunWith;
+import com.pastillasCreator.pill_box.almacenaje.AppointmentAccumulator;
+import com.pastillasCreator.pill_box.creadorElementosCalendario.AppointmentCreator;
+import com.pastillasCreator.pill_box.herramientas.DateManipulator;
 
-import cucumber.api.PendingException;
+import junit.framework.Assert;
+
+import java.time.LocalDateTime;
+
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
 
 public class evitarRepetirCitasTest {
-
+    private AppointmentAccumulator accumulator = AppointmentAccumulator.getAccumulator();
+    private LocalDateTime time = DateManipulator.dateFromStringToLocalDateTime("10/05/2022", "23:30");
     @Given("^una cita ya creada$")
     public void una_cita_ya_creada()  {
+        AppointmentCreator creator = new AppointmentCreator("creador", "descripcion", time);
+        accumulator.saveElement(creator.createElement());
 
     }
-
     @When("^se intente crear una cita ya hecha$")
-    public void se_intente_crear_una_cita_ya_hecha() throws Throwable {
-        throw new PendingException();
+    public void se_intente_crear_una_cita_ya_hecha() {
+        AppointmentCreator creator = new AppointmentCreator("creador", "descripcion", time);
+        accumulator.saveElement(creator.createElement());
     }
 
     @Then("^esta no se creara$")
-    public void esta_no_se_creara() throws Throwable {
-        throw new PendingException();
+    public void esta_no_se_creara()  {
+        Assert.assertEquals(1,accumulator.getAllElements().size());
     }
 
 
